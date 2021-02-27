36f7b5377184b56fdad8a2a20785796410238395
Ejaz Ali <ejazali4@protonmail.ch>
2021-02-27 23:12:00 +0000

    💄 Moved time to top-left.

--- src/widgets.ts
+++ src/widgets.ts
@@ -3,6 +3,6 @@ import Time from '../plugins/date-and-time';
 export default {
   'date-and-time': {
     component: Time,
-    defaultPosition: 'center-center'
+    defaultPosition: 'top-left'
   }
 };
