d0ade828eff4322dc42766c4a2037eeeecd9eae9
Ejaz Ali <ejazali4@protonmail.ch>
2021-04-11 21:10:23 +0100

    💄 Settings UI

--- html/js/settings.js
+++ html/js/settings.js
@@ -9,6 +9,7 @@ const settingsOpen = () => {
                 document.getElementById('settings-loaded').setAttribute('style', '');
                 document.getElementById('settings-loaded').setAttribute('style', '');
                 document.getElementsByClassName('settings-skel')[0].setAttribute('style', 'display: none');
+                postSettingsInit();
             });
         } else { 
             netError(response.status);
@@ -17,4 +18,19 @@ const settingsOpen = () => {
 }
 const settingsClose = () => {
     document.getElementById('settings-container').setAttribute('class', 'settings-container settings-close');
+}
+const postSettingsInit = () => {
+    window["checked"] = 0;
+    refreshChecks();
+}
+
+const refreshChecks = () => {
+    if (window["checked"] == 0) {
+        document.getElementsByClassName("checkbox-big")[0].setAttribute("class", "checkbox-big");
+        document.getElementsByClassName("checkbox-big")[1].setAttribute("class", "checkbox-big checkbox-inactive");
+    }
+    if (window["checked"] == 1) {
+        document.getElementsByClassName("checkbox-big")[1].setAttribute("class", "checkbox-big");
+        document.getElementsByClassName("checkbox-big")[0].setAttribute("class", "checkbox-big checkbox-inactive");
+    }
 }
