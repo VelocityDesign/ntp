cb029b748a9aabc0ecf952440e34f0682d6cc3f6
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-12 22:44:59 +0100

    🔨 Fix version script

--- scripts/version.js
+++ scripts/version.js
@@ -1,9 +1,8 @@
 const { readFileSync, writeFileSync } = require('fs')
 const path = require('path')
 
-const ROOT = path.join(__dirname, '..')
+const ROOT = path.join(process.cwd(), '..')
 const PACKAGE_JSON = path.join(ROOT, 'package.json')
-const MANIFEST = path.join(ROOT, 'src', 'constants', 'manifest.json')
 
 const version = process.argv[2]
 console.log(version)
@@ -11,7 +10,3 @@ console.log(version)
 let package_json = JSON.parse(readFileSync(PACKAGE_JSON).toString())
 package_json.version = version
 writeFileSync(PACKAGE_JSON, JSON.stringify(package_json))
-
-let manifest = JSON.parse(readFileSync(MANIFEST).toString())
-manifest.version = version
-writeFileSync(MANIFEST, JSON.stringify(manifest))
