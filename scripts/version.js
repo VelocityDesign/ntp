7f5f4e8de9c551e7f7f083d3f21cbea9c4f6ea9c
trickypr <trickypr@icloud.com>
2021-05-14 20:31:46 +1000

    👷 Update release ci

--- scripts/version.js
+++ scripts/version.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 const ROOT = path.join(process.cwd());
 const PACKAGE_JSON = path.join(ROOT, 'package.json');
+const MANIFEST_PATH = path.join(
+  ROOT,
+  'extensions',
+  'official',
+  'manifest.json'
+);
 
 const version = process.argv[2];
 console.log(version);
@@ -10,3 +16,7 @@ console.log(version);
 let package_json = JSON.parse(readFileSync(PACKAGE_JSON).toString());
 package_json.version = version;
 writeFileSync(PACKAGE_JSON, JSON.stringify(package_json, null, 2));
+
+let manifest = JSON.parse(readFileSync(MANIFEST_PATH).toString());
+manifest.version = version;
+writeFileSync(MANIFEST_PATH, JSON.stringify(manifest, null, 2));
